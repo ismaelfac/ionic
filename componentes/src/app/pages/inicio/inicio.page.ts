82eb365af60876105a5d796c6361a8ed2d754420
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2020-03-16 10:47:59 -0500

    Base Project

---
+++ componentes/src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      RedirectTo: '/action-sheet'
+    },
+    {
+      icon: 'appstore',
+      name: 'Alert',
+      RedirectTo: '/alert'
+    }
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  RedirectTo: string;  
+}
